0be8666efe36e1f330dba83089c79ff3df87fb75
Wiston Lamshing <wlamshing@gmail.com>
2023-08-22 09:50:51 -0500

    defined variables file
    Resources are defined

--- main.tf
+++ main.tf
@@ -17,9 +17,9 @@ variable "azurerm_resource_group" "main" {
 }
 
 module "Key_vaults" {
-    source = ".keyvault_module"
+    source          = ".keyvault_module"
 
-    environments = var.environments
-    resource_group = azurerm_resource_group.main.name
+    environments    = var.environments
+    resource_group  = azurerm_resource_group.main.name
     subscription_id = "" # Replace with own subscription ID
 }
