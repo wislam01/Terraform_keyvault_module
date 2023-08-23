f93cd59296b5db6b57dd30b23dffab50b6ee185f
Wiston Lamshing <wlamshing@gmail.com>
2023-08-22 20:26:52 -0500

    removed some variables

--- main.tf
+++ main.tf
@@ -1,19 +1,12 @@
 provider "azurerm" {
     features {}
-    client_id       = var.client_id
-    client_secret   = var.client_secret
     subscription_id = var.subscription_id
     tenant_id       = var.tenant_id
 }
 
-module "key_vaults" {
+module "keyvaults" {
     source               = "./key_vault_module"
     environments         = var.environments
-    resource_group_name  = var.resource_group
-    subscription_id      = var.subscription_id
-    tenant_id            = var.tenant_id
-}
-
-output "retrieved_keyvault_ids" {
-  value = module.key_vaults.keyvault_ids
+    resource_group       = var.resource_group_name
+    vnet_subnet_id       = var.vnet_subnet_id
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,6 +1,5 @@
-tenant_id             = "your-tenant-id"
-client_id             = "your-client-id"
-client_secret         = "your-client-secret"
 subscription_id       = "your-subscription-id"
-resource_group        = "resource-group"
+resource_group_name        = "resource-group"
 environments          = ["prod", "nonprod", "dev", "test", "qa", "perf"]
+vnet_subnet_id = "/subscription/your-subscription-id/resourceGroups/your-resource-group/providers/Microsoft.Network/virtualNetwork/your-vnet/subnets/your-subnet"
+
