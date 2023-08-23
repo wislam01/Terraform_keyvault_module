d8a8b8e26585ae12edc02796f3f4e4559398ff26
Wiston Lamshing <wlamshing@gmail.com>
2023-08-22 21:00:29 -0500

    made adjustments to remove outside module

--- key_vault_module/main.tf
+++ key_vault_module/main.tf
@@ -3,51 +3,37 @@ resource "azurerm_resource_group" "main" {
     location = "East US"
 }
 
-module "private_endpoints" {
-    source = "Azure/private-endpoints/azurerm"
-    resource_group_name = var.resource_group_name
-    subnet_id = var.vnet_subnet_id
-
-    private_link_service_connections = [
-        for env in var.environments :
-        {
-            name = "${env}-keyvault"
-            private_link_service_id = azurerm_key_vault.keyvaults[env].id
-            is_manual_connection = false
-            private_connection_resource_id = azurerm_key_vault.keyvaults[env].id
-            subresource_names = ["vault"]
-        }
-    ]
-}
-
 resource "azurerm_key_vault" "keyvaults" {
-    for_each            = tosset(var.environments)
+    for_each            = toset(var.environments)
     name                = format("%s-keyvault", each.key)
     location            = azurerm_resource_group.main.location
     resource_group_name = azurerm_resource_group.main.name
     sku_name            = "standard"
 }
 
-/* resource "azurerm_private_dns_zone" "private_dns_zones" {
-    count                = length(var.environments)
-    name                 = format("%s-privatelink.valtcore.azure.net", var.environments[count.index])
-    resource_group  = azurerm_resource_group.main.name
-} */
+resource "azurerm_private_dns_zone" "private_dns_zones" {
+    for_each = azurerm_key_vault.keyvaults
+    name                 = "${each.key}-privatelink.vaultcore.azure.net"
+    resource_group_name  = azurerm_resource_group.main.name
+    registration_enabled = true
+}
 
-/* resource "azurerm_private_endpoint" "private_endpoints" {
-    count                            = length(var.environments)
-    name                             = format("%s-keyvault-endpoint", var.environments[count.index])
+resource "azurerm_private_endpoint" "private_endpoints" {
+    for_each                 = azurerm_key_vault.keyvaults
+    name                             = "${each.key}-keyvault-endpoint"
     location                         = resource_group.main.location
     resource_group              = azurerm_resource_group.main.name
-    subnet_id                        = "/subscription/${var.subscription_id}/resourceGroups/${var.resource_group}/providers/Microsoft.Network/virtualNetworks/my-vnet/subnets/my-subnet"
+    subnet_id                        = var.vnet_subnet_id
+    private_dns_zone_group {
+      name = "${each.key}-pdzg"
+      private_dns_zone_group = [azurerm_private_dns_zone.private_dns_zones[each.key].id]
+    }
+
     private_service_connection {
-      name                           = format("%s-keyvault-connection", var.environment[count.index])
+      name                           = "#{each.key}-keyvault-connection"
       is_manual_connection           = false
-      private_connection_resource_id = azurerm_key_vault.keyvaults[count.index].id
+      private_connection_resource_id = azurerm_key_vault.keyvaults[each.key].id
       subresource_names              = ["vault"]
     }
-} */
-
-output "keyvault_ids" {
-    value = {for env in var.var.environments : env => azurerm_key_vault.keyvaults[env].id}
 }
+
