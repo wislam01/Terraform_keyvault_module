40b02dec754f83bf97ceb2a26cc23e7af6dcb572
Wiston Lamshing <wlamshing@gmail.com>
2023-08-23 13:11:39 -0500

    terraform format

--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
-variable "environments"{
-    description = "List of envonments needed for the Key Vault"
-    type        = list(string)
+variable "environments" {
+  description = "List of envonments needed for the Key Vault"
+  type        = list(string)
 }
 
 variable "resource_group_name" {
-    description = "Name of the resource group"
-    type        = string
+  description = "Name of the resource group"
+  type        = string
 }
 
 variable "subscription_id" {
-    description = "ID of the Azure subscription"
-    type        = string
+  description = "ID of the Azure subscription"
+  type        = string
 }
 
 variable "tenant_id" {
-    description = "ID of the Azure AD tenant"
-    type        = string
+  description = "ID of the Azure AD tenant"
+  type        = string
 }
 
 variable "vnet_subnet_id" {
