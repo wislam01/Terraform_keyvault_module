b2457e1c5fa62d41ade16a0ec25d6d518db80039
Wiston Lamshing <wlamshing@gmail.com>
2023-08-22 22:26:26 -0500

    adjusted definitions on variables

--- key_vault_module/outputs.tf
+++ key_vault_module/outputs.tf
@@ -1,3 +1,7 @@
 output "keyvault_ids" {
-    value = {for env in var.var.environments : env => azurerm_key_vault.keyvaults[env].id}
+    value = {for env in var.environments : env => azurerm_key_vault.keyvaults[env].id}
+}
+
+output "private_endpoint_ids" {
+    value = azurerm_private_endpoint.private_endpoints[*].id
 }
